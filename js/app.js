ae7dc3948d2f39f921fc6557b627b6254174b711
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-09-18 13:03:57 -0300

    Projeto nao finalizado

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((Letra, i) => {
+        setTimeout(() => elemento.innerHTML += Letra, 75 * i);
+    });
+}
+
+const titulo = document.querySelector('.titulo-primeira-section');
+typeWriter(titulo);
+
+typeWriter(document.querySelector('.descricao-primeira-section'));
